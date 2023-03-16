84ccfaef6c46f8554c911ccb7d2d76a8d0b426a7
Yusuf <79724222+altundalyusuf@users.noreply.github.com>
2023-03-16 18:58:03 +0300

    Add files via upload

---
+++ yazilimci fabrikasi/hafta 7 - redux/redux-shop/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" exact element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
